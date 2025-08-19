f860fdf84984692c4d066e0ec4a25d418a4773fe
vinith-15 <vinivinith893@gmail.com>
2025-08-19 20:01:50 +0530

    updated files

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    ' whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,8 +121,7 @@ MEDIA_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS=[
 	BASE_DIR/'static'
 ]
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
